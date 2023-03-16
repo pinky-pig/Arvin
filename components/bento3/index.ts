182dc284ac876e25526685081ad10e9e8aba9593
pinky-pig <2545589993@qq.com>
2023-03-16 19:36:21 +0800

    feat: add bento

--- components/bento3/index.ts
+++ components/bento3/index.ts
@@ -1,7 +1,5 @@
-/* eslint-disable no-console */
 import type { Ref } from 'vue'
 import { getArea } from './utils/area'
-import { hitTest } from './utils/hitTest'
 
 export interface GridCellsType {
   id: string
@@ -38,17 +36,14 @@ export function initGridContainer(
     currentClickedElement.value = getCellObjectInStoreFromPosition(mouseFrom)
     if (currentClickedElement.value) {
       isDragging = true
-      console.log('开始拖拽')
       // 提供一个占位的
-      proxyBox.value = Object.assign({}, currentClickedElement.value)
+      proxyBox.value = Object.assign({ tag: 'proxy' }, currentClickedElement.value)
       const index = gridCells.value.findIndex((ele: { id: any }) => ele.id === currentClickedElement.value.id)
       if (index !== -1) {
         const ele = gridCells.value.splice(index, 1)
         gridCells.value.push(ele[0])
       }
     }
-
-    area = getArea(gridCells.value)
   }
   function mousemove(e: MouseEvent) {
     mouseTo = { x: e.clientX, y: e.clientY }
@@ -71,119 +66,57 @@ export function initGridContainer(
         currentClickedElement.value.y = 1
       mouseFrom = { x: e.clientX, y: e.clientY }
 
-      // 获取proxy的位置，然后判断是否碰撞到其他的元素
-      // const ele = getElementAtPosition(
-      //   (currentClickedElement.value.x + currentClickedElement.value.width / 2) * cellBox.width,
-      //   (currentClickedElement.value.y + currentClickedElement.value.height / 2) * cellBox.height,
-      // )
-      // ele && console.log(ele, '碰撞的要素')
-
-      // 1.碰到其他元素后，就按照下面的走
-      // const minDistanceEle = getMinDistanceElementFromCurrentElement()
-      // minDistanceEle && console.log(minDistanceEle, '最近的一个要素')
-
-      // 1.将当前要素的占位添加到这个位置，currentClickedElement.value.x = ele.x; currentClickedElement.value.y = ele.y
-      // 2.將碰撞到的 ele 的位置向下移动， ele.y += currentClickedElement.value.height
-      // 3.原来的位置就空出来了，需要补上
-      // -- 3.1 遍历所有的元素，找到其下面的那个元素。
-      // 勾股定理，每个元素中心点 和 currentClickedElement.value 中心点的距离最小的，并且 y 坐标大于 currentClickedElement.value.y 的元素
-      // -- 3.2 将这个元素的 y 坐标减去 currentClickedElement.value.height
-
-      // 2.碰不到其他元素，就直接添加到位置最上
-      // if (!ele) {
-
-      // }
-
-      // 1. 四舍五入后
       proxyBox.value.x = Math.round(currentClickedElement.value.x)
       proxyBox.value.y = Math.round(currentClickedElement.value.y)
 
       /////////////////////////////////////////////////////////////////////////////////////
-      // 2. 如果没有碰撞，那么就一直找到最上面的那个位置
-      // 3. 如果有碰撞，那么就找到碰撞的元素，然后将当前的元素添加到这个位置
-      const objs: any = []
+
+      // 所有元素，当前的元素用代理元素替换
+      const allCellsWithProxyByCurrent: any = []
       gridCells.value.forEach((item) => {
-        item.id !== currentClickedElement.value.id && objs.push(item)
+        if (item.id === currentClickedElement.value.id)
+          allCellsWithProxyByCurrent.push(proxyBox.value)
+        else
+          allCellsWithProxyByCurrent.push(item)
       })
-      area = getArea(objs)
 
-      // proxyBox.value 的 x 就是第几列 在x方向上是第几
-      // proxyBox.value 的 y 就是第几行 在y方向上是第几
-      // console.log(proxyBox.value)
+      // 开始拖拽重排
+      rearrange(proxyBox.value)
+      function rearrange(node: any) {
+        // 1.如果有碰撞，碰撞的这一行及下面的所有的要素，全部都下移
+        gridCells.value.forEach((n) => {
+          if (node.id !== n.id && checkHit(node, n)) {
+            gridCells.value.forEach((n) => {
+              if (node.id !== n.id) {
+                if (n.y >= node.y)
+                  n.y += node.height
+              }
+            })
+          }
+        })
 
-      // 遍历每一个元素，如果其上面有值，那么就将其下移，如果没有，将移上去
-      gridCells.value.forEach((n: any) => {
-        if (n.id !== currentClickedElement.value.id) {
+        // 2.将每个元素冒泡放置最顶层
+        area = getArea(allCellsWithProxyByCurrent)
+
+        allCellsWithProxyByCurrent.forEach((n: any) => {
           const y = bubbleUp(n)
           if (y < n.y)
             n.y = y
-        }
-      })
-
-      // 代理元素
-      const y = bubbleUp(proxyBox.value)
-      if (y < proxyBox.value.y)
-        proxyBox.value.y = y
-
-      // 从下往上找，一直找到最上层空值的位置
+        })
+      }
       function bubbleUp(node: any) {
         for (let row = node.y - 1; row > 0; row--) {
-        // 如果一整行都为空，则直接继续往上找
+          // 如果一整行都为空，则直接继续往上找
           if (area[row] === undefined)
             continue
           for (let col = node.x; col < node.x + node.width; col++) {
-          // 改行如果有内容，则直接返回下一行
+            // 改行如果有内容，则直接返回下一行
             if (area[row][col] !== undefined)
               return row + 1
           }
         }
         return 1
       }
-
-      ///////////////////////////////////////////////////////////////////////////////
-
-      overlap(proxyBox.value)
-      function overlap(node: any) {
-        // 碰撞检测，查找一起碰撞节点里面，位置最靠上的那个
-        gridCells.value.forEach((n) => {
-          if (node.id !== n.id) {
-            if (checkHit(node, n)) {
-              // 如果有碰撞，碰撞的这一行及下面的所有的要素，前部都下移
-              gridCells.value.forEach((n) => {
-                if (node.id !== n.id) {
-                  if (n.y >= node.y)
-                    n.y += node.height
-                }
-              })
-            }
-          }
-        })
-
-        area = getArea([...objs, proxyBox.value])
-        console.log(area)
-
-        gridCells.value.forEach((n: any) => {
-          if (n.id !== currentClickedElement.value.id) {
-            const y = bubbleUp(n)
-            if (y < n.y)
-              n.y = y
-          }
-        })
-        function bubbleUp(node: any) {
-          for (let row = node.y - 1; row > 0; row--) {
-            // 如果一整行都为空，则直接继续往上找
-            if (area[row] === undefined)
-              continue
-            for (let col = node.x; col < node.x + node.width; col++) {
-              // 改行如果有内容，则直接返回下一行
-              if (area[row][col] !== undefined)
-                return row + 1
-            }
-          }
-          return 1
-        }
-      }
-
       function checkHit(node1: any, node2: any) {
         if (
           node1.x < node2.x + node2.width
@@ -207,7 +140,6 @@ export function initGridContainer(
     mouseFrom.x = 0
     mouseFrom.y = 0
     isDragging = false
-    console.log('拖拽结束')
   }
   // 获取当前点击的元素
   function getCellObjectInStoreFromPosition(position: { x: number; y: number }): Object | null {
@@ -218,63 +150,5 @@ export function initGridContainer(
       result = gridCells.value.filter((ele: { id: string }) => ele.id === initElement.id)
 
     return result ? result[0] : null
-  }
-
-  /**
-   * 获取这个位置的要素
-   * @param x x 坐标
-   * @param y y 坐标
-   * @returns 要素
-   */
-  function getElementAtPosition(x: number, y: number): any {
-    let hitElement: any | null = null
-
-    // We need to to hit testing from front (end of the array) to back (beginning of the array)
-
-    const objs: any = [proxyBox.value]
-    gridCells.value.forEach((item) => {
-      item.id !== currentClickedElement.value.id && objs.push({
-        ...item,
-        x: item.x * cellBox.width,
-        y: item.y * cellBox.height,
-        width: item.width * cellBox.width,
-        height: item.height * cellBox.height,
-      })
-    })
-    for (let i = objs - 1; i >= 0; --i) {
-      if (hitTest(objs, x, y)) {
-        hitElement = objs[i]
-        break
-      }
-    }
-    return hitElement
-  }
-
-  function getMinDistanceElementFromCurrentElement() {
-    // 勾股定理，每个元素中心点 和 proxyBox.value 中心点的距离最小的，并且 y 坐标大于 proxyBox.value.y 的元素
-    let hitElement: any | null = 0
-    let minDistance = 0
-    for (let i = gridCells.value.length - 1; i >= 0; --i) {
-      // 不是当前点击的元素
-
-      if (gridCells.value[i].id !== proxyBox.value.id) {
-        const a = gridCells.value[i].x - proxyBox.value.x
-        const b = gridCells.value[i].y - proxyBox.value.y
-        const distance = Math.sqrt(a ** 2 + b ** 2)
-
-        if (i === 0) {
-          minDistance = distance
-          hitElement = gridCells.value[i]
-        }
-
-        // 这里还需要判断 y 值的大小
-        if (minDistance > distance) {
-          minDistance = distance
-          hitElement = gridCells.value[i]
-        }
-      }
-    }
-    console.log(hitElement)
-    return hitElement
   }
 }
