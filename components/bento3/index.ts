cc018751d46831e3b8dec456248a987a226fd738
pinky-pig <2545589993@qq.com>
2023-03-17 11:21:31 +0800

    feat: add bento

--- components/bento3/index.ts
+++ components/bento3/index.ts
@@ -70,40 +70,98 @@ export function initGridContainer(
       proxyBox.value.y = Math.round(currentClickedElement.value.y)
 
       /////////////////////////////////////////////////////////////////////////////////////
-
-      // 所有元素，当前的元素用代理元素替换
+      // 除了当前拖拽的元素之外的所有元素
       const allCellsWithProxyByCurrent: any = []
       gridCells.value.forEach((item) => {
-        if (item.id === currentClickedElement.value.id)
-          allCellsWithProxyByCurrent.push(proxyBox.value)
-        else
+        if (item.id !== currentClickedElement.value.id)
           allCellsWithProxyByCurrent.push(item)
       })
+      // 判断当前拖拽的元素的占位位置，应该冒泡找到最顶的位置
+      area = getArea(allCellsWithProxyByCurrent)
+      const y = bubbleUp(proxyBox.value)
+      if (y < proxyBox.value.y)
+        proxyBox.value.y = y
 
-      // 开始拖拽重排
-      rearrange(proxyBox.value)
-      function rearrange(node: any) {
-        // 1.如果有碰撞，碰撞的这一行及下面的所有的要素，全部都下移
-        gridCells.value.forEach((n) => {
+      // 这里使用一个递归的方式，拖拽碰撞之后将所有的碰撞的元素都下移
+      hitAllTest(proxyBox.value, allCellsWithProxyByCurrent)
+      function hitAllTest(node: any, allNodes: any) {
+        const hittedNodes: any = []
+        allNodes.forEach((n: any) => {
           if (node.id !== n.id && checkHit(node, n)) {
-            gridCells.value.forEach((n) => {
-              if (node.id !== n.id) {
-                if (n.y >= node.y)
-                  n.y += node.height
-              }
-            })
+            hittedNodes.push(n)
+            n.y += node.height
           }
         })
 
-        // 2.将每个元素冒泡放置最顶层
-        area = getArea(allCellsWithProxyByCurrent)
-
-        allCellsWithProxyByCurrent.forEach((n: any) => {
-          const y = bubbleUp(n)
-          if (y < n.y)
-            n.y = y
-        })
+        if (hittedNodes.length > 0) {
+          hittedNodes.forEach((n: any) => {
+            hitAllTest(n, allNodes)
+          })
+        }
       }
+
+      // 所有碰撞的元素移动之后，将每个元素遍历一边，然后将其冒泡到顶部
+      const allCellsWithProxy = [...allCellsWithProxyByCurrent, proxyBox.value]
+      area = getArea(allCellsWithProxy)
+      allCellsWithProxyByCurrent.forEach((n: any) => {
+        const y = bubbleUp(n)
+        if (y < n.y)
+          n.y = y
+      })
+
+      // if (flag) {
+      //   gridCells.value.forEach((n) => {
+      //     if (proxyBox.value.id !== n.id) {
+      //       if (n.y >= proxyBox.value.y)
+      //         n.y += proxyBox.value.height
+      //     }
+      //   })
+      // }
+
+      // 所有元素，当前的元素用代理元素替换
+      // const allCellsWithProxyByCurrent: any = []
+      // gridCells.value.forEach((item) => {
+      //   if (item.id === currentClickedElement.value.id)
+      //     allCellsWithProxyByCurrent.push(proxyBox.value)
+      //   else
+      //     allCellsWithProxyByCurrent.push(item)
+      // })
+
+      // // 开始拖拽重排
+      // area = getArea(allCellsWithProxyByCurrent)
+      // const y = bubbleUp(proxyBox.value)
+      // if (y < proxyBox.value.y)
+      //   proxyBox.value.y = y
+      // area = getArea(allCellsWithProxyByCurrent)
+
+      // rearrange()
+      // function rearrange() {
+      //   // // 1.如果有碰撞，碰撞的这一行及下面的所有的要素，全部都下移
+      //   let flag = false
+
+      //   gridCells.value.forEach((n) => {
+      //     if (proxyBox.value.id !== n.id && checkHit(proxyBox.value, n)) {
+      //       // 因为只能碰撞一个，所以只要碰撞了，就可以直接下移了
+      //       flag = true
+      //     }
+      //   })
+
+      //   if (flag) {
+      //     gridCells.value.forEach((n) => {
+      //       if (proxyBox.value.id !== n.id) {
+      //         if (n.y >= proxyBox.value.y)
+      //           n.y += proxyBox.value.height
+      //       }
+      //     })
+      //   }
+
+      //   area = getArea(allCellsWithProxyByCurrent)
+      //   allCellsWithProxyByCurrent.forEach((n: any) => {
+      //     const y = bubbleUp(n)
+      //     if (y < n.y)
+      //       n.y = y
+      //   })
+      // }
       function bubbleUp(node: any) {
         for (let row = node.y - 1; row > 0; row--) {
           // 如果一整行都为空，则直接继续往上找
