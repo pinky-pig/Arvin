56b26a904d15717027b29294c36997af83688d61
pinky-pig <2545589993@qq.com>
2023-03-28 19:09:09 +0800

    fix: 引入 mapbox css 问题

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -15,18 +15,31 @@ const size = ref(180)
 const gap = ref(10)
 
 const containerRef = ref(null)
+
+// 找出 bentoCells 中最大的宽度
+
 useResizeObserver(containerRef, (entries) => {
   const entry = entries[0]
   const { width } = entry.contentRect
 
+  // todo: 这里最好修改，如果当前的宽度除以当前的尺寸，
+  // 比如 2 * size < width < 3 * size
+  // 那么就缩小这个 size ，正好能显示三个
+  // if (width < size.value * 2 + gap.value * 2) {
+  //   // 当前的宽度小于两个 cell 的宽度，那么就显示两个，但是修改尺寸为正好放下两个的尺寸
+  //   size.value = width / 2 - gap.value
+  //   maximumCells.value = 2
+  // }
+  // else {
   if (Math.ceil(width / (size.value + gap.value)) <= 6) {
-    maximumCells.value = Math.ceil(width / (size.value + gap.value))
+    maximumCells.value = Math.floor(width / (size.value + gap.value))
     size.value = 180
   }
   else {
     maximumCells.value = 6
     size.value = 240
   }
+  // }
 })
 </script>
 
