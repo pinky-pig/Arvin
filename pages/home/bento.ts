88baf5a256cb31c9961aa7154b483cf5a39fef65
pinky-pig <2545589993@qq.com>
2023-03-30 11:47:30 +0800

    fix: cells position

--- pages/home/bento.ts
+++ pages/home/bento.ts
@@ -6,6 +6,6 @@ import Dark from '~/components/dark/Dark.vue'
 export const bentoCells = [
   { id: '1', x: 0, y: 0, width: 2, height: 2, component: markRaw(Profile) },
   { id: '2', x: 2, y: 0, width: 2, height: 1, component: markRaw(Mapbox) },
-  { id: '3', x: 3, y: 2, width: 1, height: 1, component: markRaw(CountDown) },
-  { id: '4', x: 2, y: 2, width: 1, height: 1, component: markRaw(Dark) },
+  { id: '3', x: 2, y: 1, width: 1, height: 1, component: markRaw(CountDown) },
+  { id: '4', x: 3, y: 1, width: 1, height: 1, component: markRaw(Dark) },
 ]
