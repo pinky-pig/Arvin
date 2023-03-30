ea2330a9ba097d475c99769150013ea7ca80617a
pinky-pig <2545589993@qq.com>
2023-03-30 16:25:50 +0800

    feat: 修改头部样式

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="nav-bg" style="transform: translate3d(0px, 0px, 0px);" />
+</template>
+
+<style scoped>
+#nav-bg {
+  position: fixed;
+  z-index: 9993;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  will-change: opacity,transform;
+  backdrop-filter: saturate(50%) blur(8px);
+  -webkit-backdrop-filter: saturate(50%) blur(8px);
+}
+
+@media (min-width: 768px) {
+  #nav-bg {
+    height: 108px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <main class="py-20 px-5 text-center">
+  <main class="pt-24 md:pt-40 pb-20 px-5 text-center">
+    <Header />
     <slot />
     <Footer />
     <div class="mt-5 mx-auto text-center opacity-25 text-sm">
