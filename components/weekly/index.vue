8a46cd08e9cabfe717132a46d6e7204ac125801a
pinky-pig <2545589993@qq.com>
2023-04-13 18:38:18 +0800

    fix: 设置图片的大小， lighthouse

--- components/weekly/index.vue
+++ components/weekly/index.vue
@@ -5,9 +5,16 @@ const color = useColorMode()
 
 <template>
   <div class="relative select-none bg-[var(--card--bg)] overflow-hidden">
-    <img class="select-none pointer-events-none" :style="{ opacity: color.value === 'dark' ? 0 : 1 }" src="https://nevflynn.com/images/wrap-bg.svg" alt="">
+    <img
+      class=" object-fill select-none pointer-events-none"
+      :style="{ opacity: color.value === 'dark' ? 0 : 1 }"
+      src="/wrap-bg.svg"
+      alt=""
+    >
     <img
       class="
+        w-full
+        h-auto
         object-fill
         select-none
         pointer-events-none
