47540b500780aec2c257d03a0f5da68564bcbc35
pinky-pig <2545589993@qq.com>
2023-04-14 17:45:17 +0800

    feat: 添加拟物化卡片

--- pages/404.vue
+++ pages/404.vue
@@ -1,29 +1,3 @@
 <template>
-  <button
-    class="
-      rounded-md
-      border
-      p-2
-      outline-none
-      dark:border-gray-900/50 dark:bg-gray-700 dark:text-gray-100
-    "
-    @click="useToggleTheme"
-  >
-    404
-  </button>
-
-  <div
-    class="
-      w-370px
-      h-180px
-      m-l-50
-      overflow-hidden
-      bg-dark-300
-    "
-  >
-    <img
-      src="https://img1.baidu.com/it/u=413643897,2296924942&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500"
-      style="transform: rotate(330deg) translate3d(100px, 10px, 10px); border-radius: 0.375rem;"
-    >
-  </div>
+  <NeumorphismCard />
 </template>
