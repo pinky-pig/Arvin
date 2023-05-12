3d4709353f3b411cb45f393cb6136f9903359288
pinky-pig <2545589993@qq.com>
2023-05-12 12:24:13 +0800

    feat: 添加头部导航

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,59 @@
 <script setup lang="ts">
+const navFilter = [
+  { label: '项目', route: '/' },
+  { label: '路由', route: '/' },
+  { label: '归档', route: '/' },
+]
 
+const currentItem = ref<typeof navFilter[0] | null>()
+const currentItemBgRef = ref<HTMLElement>()
+// 选中当前选项卡
+function handleActiveTab(item: typeof navFilter[0], index: number) {
+  const animateDom = document.querySelectorAll('.animate-dom')[0] as HTMLElement
+  animateDom.classList.remove('animate-jello')
+  currentItem.value = item
+  currentItemBgRef.value!.style.transform = `translate(${(document.querySelectorAll('.filter-option')[index] as HTMLElement).offsetLeft + 5}px, 5px)`
+  animateDom.classList.add('animate-jello')
+}
 </script>
 
 <template>
-  <div id="nav-bg" class="flex justify-start flex-row items-center" style="transform: translate3d(0px, 0px, 0px);">
+  <div
+    id="nav-bg"
+    class="flex flex-row justify-between items-center"
+  >
     <img class="w-56px h-56px" src="/logo.svg" alt="">
+
+    <div class="nav-filter">
+      <div
+        v-for="item, index in navFilter"
+        :key="item.label"
+        class="filter-option "
+        @click="handleActiveTab(item, index)"
+      >
+        {{ item.label }}
+      </div>
+
+      <div
+        ref="currentItemBgRef"
+        class="
+          w-70px h-32px
+          absolute top-0 left-0 z-0
+          pointer-events-none
+          transition-transform
+          duration-300
+          ease-in-out
+          translate-x-8px
+          translate-y-5px
+          -z-1
+        "
+      >
+        <div class="animate-dom animate-jello w-full h-full">
+          &nbsp;
+        </div>
+      </div>
+    </div>
+    <div class="w-40px h-40px" />
   </div>
 </template>
 
@@ -33,6 +82,68 @@
 @media (min-width: 768px) {
   #nav-bg {
     height: 70px;
+  }
+}
+.nav-filter{
+  position: relative;
+  background: transparent;
+  border: 2px solid var(--nav-placeholder-bg);
+  border-radius: 23px;
+  display: flex;
+  font-family: "Silka Medium";
+  font-size: 14px;
+  padding: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.filter-option{
+  width: 80px;
+  height: 32px;
+  line-height: 32px;
+  cursor: pointer;
+  border-radius: 50px;
+  -webkit-box-align: center;
+  padding: 0px 16px;
+  transition: opacity 0.3s ease 0s;
+  color: var(--nav-text);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  user-select: none;
+}
+.animate-dom{
+  border-radius: 50px;
+  background-color: var(--nav-placeholder-bg);
+  color: var(--nav-text);
+}
+.animate-jello {
+  animation: jello-horizontal 0.9s both;
+}
+@keyframes jello-horizontal{
+  0% {
+    transform: scale3d(1, 1, 1);
+  }
+  30% {
+    transform: scale3d(1.25, 0.75, 1);
+  }
+  40% {
+    transform: scale3d(0.75, 1.25, 1);
+  }
+  50% {
+    transform: scale3d(1.15, 0.85, 1);
+  }
+  65% {
+    transform: scale3d(0.95, 1.05, 1);
+  }
+  75% {
+    transform: scale3d(1.05, 0.95, 1);
+  }
+  100% {
+    transform: scale3d(1, 1, 1);
   }
 }
 </style>
