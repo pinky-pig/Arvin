6b52bdeaea0c924f215e567a231ef4ab2ab5afaa
pinky-pig <2545589993@qq.com>
2023-05-16 18:59:12 +0800

    feat: 添加 simplebar 滚动条

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+// import Simplebar from '~/components/Simplebar.vue'
+</script>
+
 <template>
+  <!-- <Simplebar class="w-full h-full"> -->
   <main class="pt-180px md:pt-136px pb-20 px-5 text-center overflow-x-hidden min-h-screen">
     <Header />
     <slot />
     <Footer />
   </main>
+  <!-- </Simplebar> -->
 </template>
--- pages/archives/index.vue
+++ pages/archives/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+import Simplebar from '~/components/Simplebar.vue'
 </script>
 
 <template>
   archives
+  <Simplebar class="w-200px h-500px bg-gray-200">
+    <div class="w-200px h-1000px bg-red-200 flex flex-col justify-between">
+      <div>666</div>
+      <div>888</div>
+    </div>
+  </Simplebar>
 </template>
 
 <style scoped>
