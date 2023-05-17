edbd040eb7c918d4ccdb0d9c39e80c1c5b9de864
pinky-pig <2545589993@qq.com>
2023-05-17 16:39:46 +0800

    fix: try to fix playwright bug

--- functions/result.js
+++ functions/result.js
@@ -1,5 +1,5 @@
-import chromium from 'chrome-aws-lambda'
-import puppeteer from 'puppeteer-core'
+const chromium = require('chrome-aws-lambda')
+const puppeteer = require('puppeteer-core')
 
 exports.handler = async function (event, context) {
   const browser = await puppeteer.launch({
