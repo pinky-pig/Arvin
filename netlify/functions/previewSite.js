c844854c1e22949568ea45426adafd2deab8c522
pinky-pig <2545589993@qq.com>
2023-05-23 11:32:00 +0800

    chore: 删除 chrome-aws-lambda 依赖

--- netlify/functions/previewSite.js
+++ netlify/functions/previewSite.js
@@ -1,29 +1,36 @@
-const chromium = require('chrome-aws-lambda')
-const playwright = require('playwright-core')
+// const chromium = require('chrome-aws-lambda')
+// const playwright = require('playwright-core')
 
 exports.handler = async function (event, context) {
-  const path = await chromium.executablePath
-  // eslint-disable-next-line no-console
-  console.log(path, 'executablePath')
+  // const path = await chromium.executablePath
+  // // eslint-disable-next-line no-console
+  // console.log(path, 'executablePath')
 
-  const browser = await playwright.chromium.launch()
-  const page = await browser.newPage()
+  // const browser = await playwright.chromium.launch()
+  // const page = await browser.newPage()
 
-  await page.goto('https://spacejelly.dev/')
+  // await page.goto('https://spacejelly.dev/')
 
-  const title = await page.title()
-  const description = await page.$eval('meta[name="description"]', element => element.content)
+  // const title = await page.title()
+  // const description = await page.$eval('meta[name="description"]', element => element.content)
 
-  await browser.close()
+  // await browser.close()
 
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     status: 'Ok',
+  //     page: {
+  //       title,
+  //       description,
+  //     },
+  //   }),
+  // }
   return {
     statusCode: 200,
     body: JSON.stringify({
       status: 'Ok',
-      page: {
-        title,
-        description,
-      },
+      page: {},
     }),
   }
 }
