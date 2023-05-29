c9d2b28491b98bc8be6fa30f41c5fde27a9ce616
pinky-pig <2545589993@qq.com>
2023-05-29 16:21:03 +0800

    update work

--- pages/blog/content.vue
+++ pages/blog/content.vue
@@ -59,40 +59,38 @@ function paginationJump(path: string) {
 </script>
 
 <template>
-  <main class="flex justify-center items-center p-4">
-    <div class="relative w-full">
-      <aside class="hidden md:block flex-shrink-0 flex-grow-0">
-        <LeftSidebar
-          :current-blog="currentBlog"
-          class="fixed"
-          @blogJump="paginationJump"
-        />
-      </aside>
+  <main class="layout flex justify-center p-4">
+    <aside id="grid-left" class="grid-sidebar hidden md:block flex-shrink-0 flex-grow-0">
+      <LeftSidebar
+        :current-blog="currentBlog"
+        class="fixed"
+        @blogJump="paginationJump"
+      />
+    </aside>
 
-      <div class="heti max-w-50rem pb-20 p-0 md:ml-300px md:p-[0.8rem,2rem,4rem] flex-shrink-0 flex-grow-0">
-        <h1 class="content-title flex align-middle ">
-          {{ currentBlog.title }}
-        </h1>
+    <div id="grid-main" class="heti max-w-50rem pb-20 p-0 md:p-[0.8rem,2rem,4rem] flex-shrink-0 flex-grow-0">
+      <h1 class="content-title flex align-middle ">
+        {{ currentBlog.title }}
+      </h1>
 
-        <ContentDoc :path="currentBlog.path">
-          <template #not-found>
-            <h1>Document not found</h1>
-          </template>
-        </ContentDoc>
+      <ContentDoc :path="currentBlog.path">
+        <template #not-found>
+          <h1>Document not found</h1>
+        </template>
+      </ContentDoc>
 
-        <hr>
+      <hr>
 
-        <div class="flex justify-between items-center md:flex-row flex-col mt-2 post-footer">
-          <div class="flex-1">
-            发布日期：<a href="https://github.com/" target="_blank" title="Edit">{{ currentBlog.date }}</a>
-          </div>
-          <div class="md:mt-0 mt-4 flex-1 text-right">
-            <a v-if="currentBlog.prevPath" @click="paginationJump(currentBlog.prevPath)">上一篇 |</a>
-            <a v-if="currentBlog.nextPath" @click="paginationJump(currentBlog.nextPath)">下一篇 |</a>
-            <a @click="router.push(`/blog`)">去首页 |</a>
-            <a href="https://github.com/" target="_blank" title="Follow">Twitter</a>
-            <a href="https://github.com/" title="Star" target="_blank" class="lg:inline-block hidden"> | Github</a>
-          </div>
+      <div class="flex justify-between items-center md:flex-row flex-col mt-2 post-footer">
+        <div class="flex-1">
+          发布日期：<a href="https://github.com/" target="_blank" title="Edit">{{ currentBlog.date }}</a>
+        </div>
+        <div class="md:mt-0 mt-4 flex-1 text-right">
+          <a v-if="currentBlog.prevPath" @click="paginationJump(currentBlog.prevPath)">上一篇 |</a>
+          <a v-if="currentBlog.nextPath" @click="paginationJump(currentBlog.nextPath)">下一篇 |</a>
+          <a @click="router.push(`/blog`)">去首页 |</a>
+          <a href="https://github.com/" target="_blank" title="Follow">Twitter</a>
+          <a href="https://github.com/" title="Star" target="_blank" class="lg:inline-block hidden"> | Github</a>
         </div>
       </div>
     </div>
@@ -110,6 +108,51 @@ function paginationJump(path: string) {
   .heti h1.content-title:first-child {
     margin-bottom: 2.3rem;
     font-size: 1.8rem;
+  }
+}
+
+.layout {
+  display: grid;
+  grid-auto-flow: column;
+  overflow-x: hidden;
+}
+
+.grid-sidebar {
+  height: 100vh;
+  position: sticky;
+  top: 0;
+  padding: 0;
+  margin-left: -5px;
+}
+#grid-left {
+  position: fixed;
+  z-index: 10;
+  display: none;
+}
+#grid-main {
+  padding: 0.8rem 0.5rem 4rem 0.5rem;
+  grid-column: 2;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+@media (min-width: 50em) {
+  .layout {
+    overflow: initial;
+    grid-template-columns: 18rem minmax(0, 54em);
+    gap: 0;
+  }
+
+  #grid-main {
+    padding: 0.8rem 2rem 4rem 2rem;
+  }
+
+  #grid-left {
+    display: flex;
+    padding-left: 2rem;
+    position: sticky;
+    grid-column: 1;
   }
 }
 </style>
