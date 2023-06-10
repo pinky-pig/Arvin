b58ad6b449bc4148973a5e2ae9319ad7539073e1
pinky-pig <2545589993@qq.com>
2023-06-10 11:08:33 +0800

    refactor-blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,11 +4,20 @@ import Title from './title.vue'
 </script>
 
 <template>
-  <div
-    style=" background-color: var(--blog-bg); border: 1px solid var(--blog--border ); "
-    class="mx-auto max-w-960px rounded-xl px-10px text-start md:px-36px"
-  >
+  <div style=" background-color: var(--blog-bg); border: 1px solid var(--blog--border ); "
+    class="mx-auto max-w-960px rounded-xl px-10px text-start md:px-36px">
     <Title title="全部文章" />
-    <List />
+    <!-- <List /> -->
+
+    <div>
+      <ContentList v-slot="{ list }" path="/post">
+        <div v-for="article in list" :key="article._path">
+          {{ article.tags }}
+          {{ article.desc }}
+          {{ article.date }}
+          {{ article.title }}
+        </div>
+      </ContentList>
+    </div>
   </div>
 </template>
