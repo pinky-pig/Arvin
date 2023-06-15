d17cae4e5da50454db4c9a2cc1bf89c52feca9ec
pinky-pig <2545589993@qq.com>
2023-06-15 15:08:54 +0800

    fix: mobile blog bg

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@ const isLargeScreen = useMediaQuery('(min-width: 720px)')
 <template>
   <Transition name="slide-fade-from-up" mode="out-in">
     <main
-      class="bg-[var(--bg-main-color)]"
+      class="bg-[var(--background)] md:bg-[var(--bg-main-color)]"
       :class="isNoneLayout ? '' : 'pt-100px md:pt-100px pb-20 px-10px md:px-5 overflow-x-hidden min-h-screen md:pt-112px '"
     >
       <Header v-show="!isLargeScreen || !isNoneLayout" />
