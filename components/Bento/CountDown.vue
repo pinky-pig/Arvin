1a722adea337d2af9fc97a648edc14bf32b1e0f9
pinky-pig <2545589993@qq.com>
2023-06-17 00:22:02 +0800

    fix: countdown

--- components/Bento/CountDown.vue
+++ components/Bento/CountDown.vue
@@ -36,8 +36,7 @@ function countdownTime() {
 
   // 计算已经过去的百分比
   const percentage = ((daysGone / daysInYear) * 100).toFixed(2)
-
-  return Number(percentage)
+  return percentage
 }
 </script>
 
@@ -45,7 +44,7 @@ function countdownTime() {
   <CustomCardShadowBlock class="h-300px w-400px text-5rem">
     <div class="relative h-full w-full" style="font-family: Digital">
       <span class="absolute left-50% top-50% -translate-1/2">{{ time }}</span>
-      <span v-show="time !== 100 && time !== 100.00" class="absolute left-50% top-50% text-[#0000001c] -translate-1/2 dark:text-[#ffffff1c]">00.00</span>
+      <span v-show="Number(time) !== 100 && Number(time) !== 100.00" class="absolute left-50% top-50% text-[#0000001c] -translate-1/2 dark:text-[#ffffff1c]">00.00</span>
     </div>
   </CustomCardShadowBlock>
 </template>
