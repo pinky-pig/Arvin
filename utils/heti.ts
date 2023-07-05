fbebfb7010a91655e047e74d541ce38a8977b6fa
pinky-pig <2545589993@qq.com>
2023-07-05 14:43:43 +0800

    fix: heti min js url

--- utils/heti.ts
+++ utils/heti.ts
@@ -1,7 +1,7 @@
 export async function registerHeti() {
   // 字体间距优化
   useScriptTag(
-    '/heti-addon.min.js',
+    '/heti/heti-addon.min.js',
     (el: HTMLScriptElement) => {
       const heti = new (window as any).Heti('.heti')
       heti.autoSpacing()
