4f43899b1b53cde954a8443dc3e0280759d34bbf
pinky-pig <2545589993@qq.com>
2023-07-07 15:43:32 +0800

    feat: feat: DragMotion

--- components/Bento/DragMotion.vue
+++ components/Bento/DragMotion.vue
@@ -1,51 +1,51 @@
 <script setup lang="ts">
 const isOpen = ref(false)
-
+/** ************************设置拖拽和旋转***************************** */
 const target = ref<HTMLElement>()
 const { apply } = useMotion(target, {
   initial: { rotate: 0 },
   enter: { rotate: 0 },
 })
-
 async function setRotate(rotate: number) {
   await apply({
     rotate,
   })
 }
-
 const { x, style } = useDraggable(target, {
   initialValue: { x: 40, y: 40 },
 })
+/** **************************************************************** */
 
-// 1. 拖拽过程中，通过计算拖拽的速度来计算晃动 rotate 的幅度
-// 2. 拖拽的速度越大，晃动的幅度越大
-// 3. 拖拽的速度越小，晃动的幅度也越小
-// 5. 往右移动，晃动的幅度为正数，最大为 18
-// 6. 往左移动，晃动的幅度为负数，最小为 -18
-
-const maxVelocity = 10 // 最大速度
-const maxRotation = 90 // 最大旋转角度
-const rotationFactor = 0.8 // 旋转系数，用于调整旋转幅度
-
-let lastX = computed(() => x.value).value
+/** ************************通过速度计算旋转幅度***************************** */
+let lastX = x.value
 let startTime = performance.now()
-const currentVelocity = ref(0)
-
+// 当前角度 / 当前速度 = 最大角度 / 最大速度
+// 得出：当前角度 = ( 最大角度 / 最大速度 ) * 当前速度
+// 然后再乘以一个系数，用于调整旋转幅度，角度跟速度的比例
+const cfg = {
+  maxVelocity: 10, // 最大速度
+  maxRotation: 90, // 最大旋转角度
+  rotationFactor: 0.8, // 旋转系数，用于调整旋转幅度
+}
 watch(x, () => {
-  const currentX = x.value
-  const currentTime = performance.now()
-
-  const distanceX = currentX - lastX
-  const deltaTime = currentTime - startTime
-
-  currentVelocity.value = distanceX / deltaTime
-
-  const rotationDelta = currentVelocity.value / maxVelocity * maxRotation * rotationFactor
+  const { velocity, newX, newTime } = calVelocity(lastX, x.value, startTime)
+  const { maxVelocity, maxRotation, rotationFactor } = cfg
+  const rotationDelta = velocity / maxVelocity * maxRotation * rotationFactor
   setRotate(rotationDelta)
-
-  lastX = currentX
-  startTime = currentTime
+  lastX = newX
+  startTime = newTime
 })
+
+function calVelocity(lastX: number, currentX: number, lastTime: number, currentTime = performance.now()) {
+  const distanceX = currentX - lastX
+  const deltaTime = currentTime - lastTime
+  return {
+    velocity: distanceX / deltaTime,
+    newTime: currentTime,
+    newX: currentX,
+  }
+}
+/** **************************************************************** */
 </script>
 
 <template>
