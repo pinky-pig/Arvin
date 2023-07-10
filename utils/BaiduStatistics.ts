1c4871101218942bbbcc008d73198cedc9bb0dca
pinky-pig <2545589993@qq.com>
2023-07-10 23:38:47 +0800

    chore: 添加百度统计

---
+++ utils/BaiduStatistics.ts
@@ -0,0 +1,9 @@
+export async function registerBaiduStatistics() {
+  // 字体间距优化
+  useScriptTag(
+    'https://hm.baidu.com/hm.js?31df7bf1319ac831a5a8f453821c9412',
+    (el: HTMLScriptElement) => {
+      //
+    },
+  )
+}
